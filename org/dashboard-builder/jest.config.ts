26a5b511f966be4f2a20c08b9b3c1a5a767c9801
Sakshi Mahajan <sakshi1913@gmail.com>
2024-12-24 23:11:33 +0530

    created microfrontends for different apps

---
+++ org/dashboard-builder/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: 'dashboard-builder',
+  preset: '../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nx/react/plugins/jest',
+    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nx/react/babel'] }],
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../coverage/dashboard-builder',
+};
